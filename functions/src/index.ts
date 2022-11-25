5e13f815b7d35e4d892b382e74e564692248e077
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2022-11-26 06:54:22 +0900

    完成

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+import * as functions from "firebase-functions";
+import {BlastEngine, Transaction} from "blastengine";
+const config = functions.config().blastengine;
+new BlastEngine(config.user_id, config.api_key);
+export const contact = functions.https.onCall(async (data) => {
+	const body = `${data.name}様
+お問い合わせいただきありがとうございます。
+
+内容を確認の上、早急にご返信いたします。
+
+お名前： ${data.name}
+メールアドレス： ${data.email}
+電話番号： ${data.tel}
+お問い合わせ内容：
+${data.body}
+`;
+	const transaction = new Transaction();
+	transaction
+		.setFrom(config.from_address, config.from_name)
+		.setSubject('お問い合わせいただきありがとうございます')
+		.setTo(data.email)
+		.setText(body);
+	await transaction.send();
+  // functions.logger.info(data, {structuredData: true});
+  return "success";
+});
---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
+import { connectFunctionsEmulator, getFunctions, httpsCallable } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-functions.js";
+// Firebaseの初期化
+const firebaseConfig = {
+	apiKey: "AIzaSyAawtpQ3JMiKKJyb5puRP_bCiubklKCMOA",
+	authDomain: "blastengine-form.firebaseapp.com",
+	projectId: "blastengine-form",
+	storageBucket: "blastengine-form.appspot.com",
+	messagingSenderId: "314254063614",
+	appId: "1:314254063614:web:a495c10d70a35c103eb2af"
+};
+const app = initializeApp(firebaseConfig);
+const functions = getFunctions(app);
+// ローカル開発用
+if (location.hostname === "localhost") {
+	connectFunctionsEmulator(functions, "localhost", 5001);
+}
+
+// 初期化
+document.addEventListener('DOMContentLoaded', e => {
+	const result = document.querySelector('.result')
+	result.style.display = 'none';
+	// フォーム送信
+	const form = document.querySelector('form#contact');
+	form.addEventListener('submit', async (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const data = new FormData(form);
+		const json = Object.fromEntries(data)
+		const res = await httpsCallable(functions, 'contact')(json);
+		if (res.data === 'success') {
+			result.innerText = '送信しました。お問い合わせいただきありがとうございます。';
+			result.style.display = 'block';
+			form.reset();
+		}
+	});
+});
