d5f2b40c62fd2b83eb9306ed3e281a2e1ef3c180
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2022-11-26 07:15:53 +0900

    スクリプトの体裁修正

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BlastengineとFirebase Hosting/Functionsを使ったお問い合わせフォーム
 
+## 利用法
+
+### 環境変数の設定
+
+```
+firebase functions:config:set blastengine.api_key=YOUR_API_KEY
+firebase functions:config:set blastengine.user_id=YOUR_USER_ID
+firebase functions:config:set blastengine.from_address=from@example.com
+firebase functions:config:set blastengine.from_name=Administrator
+```
+
+## デモ
+
+[https://blastengine-form.web.app/](https://blastengine-form.web.app/)
+
+## License
+
+MIT
+
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,7 @@ import {BlastEngine, Transaction} from "blastengine";
 const config = functions.config().blastengine;
 new BlastEngine(config.user_id, config.api_key);
 export const contact = functions.https.onCall(async (data) => {
-	const body = `${data.name}様
+  const body = `${data.name}様
 お問い合わせいただきありがとうございます。
 
 内容を確認の上、早急にご返信いたします。
@@ -14,13 +14,13 @@ export const contact = functions.https.onCall(async (data) => {
 お問い合わせ内容：
 ${data.body}
 `;
-	const transaction = new Transaction();
-	transaction
-		.setFrom(config.from_address, config.from_name)
-		.setSubject('お問い合わせいただきありがとうございます')
-		.setTo(data.email)
-		.setText(body);
-	await transaction.send();
+  const transaction = new Transaction();
+  transaction
+      .setFrom(config.from_address, config.from_name)
+      .setSubject("お問い合わせいただきありがとうございます")
+      .setTo(data.email)
+      .setText(body);
+  await transaction.send();
   // functions.logger.info(data, {structuredData: true});
   return "success";
 });
